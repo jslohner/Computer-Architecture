6b57f9ec01dfa11e8358508cbf90f2a03aba9fc1
Justin Lohner <lohner4@gmail.com>
2020-08-18 22:44:01 -0600

    implemented call and ret instructions and subroutine calls

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,9 +6,12 @@ instruction_codes = {
     'hlt': '0x1',
     'ldi': '0x82',
     'prn': '0x47',
+    'add': '0xa0',
     'mul': '0xa2',
     'push': '0x45',
-    'pop': '0x46'
+    'pop': '0x46',
+    'call': '0x50',
+    'ret': '0x11'
 }
 
 class CPU:
@@ -24,9 +27,12 @@ class CPU:
         self.branchtable[instruction_codes['hlt']] = self.handle_hlt
         self.branchtable[instruction_codes['ldi']] = self.handle_ldi
         self.branchtable[instruction_codes['prn']] = self.handle_prn
+        self.branchtable[instruction_codes['add']] = self.handle_add
         self.branchtable[instruction_codes['mul']] = self.handle_mul
         self.branchtable[instruction_codes['push']] = self.handle_push
         self.branchtable[instruction_codes['pop']] = self.handle_pop
+        self.branchtable[instruction_codes['call']] = self.handle_call
+        self.branchtable[instruction_codes['ret']] = self.handle_ret
 
 
     def ram_read(self, mar): # mar - [_Memory Address Register_]
@@ -112,21 +118,32 @@ class CPU:
         print(self.reg[self.ram_read(self.pc + 1)])
         self.pc += 2
 
+    def handle_add(self):
+        self.alu('ADD', self.ram_read(self.pc + 1), self.ram_read(self.pc + 2))
+        self.pc += 3
+
     def handle_mul(self): # MUL - multiply values in two registers together and store the result in registerA
         self.alu('MUL', self.ram_read(self.pc + 1), self.ram_read(self.pc + 2))
         self.pc += 3
 
     def handle_push(self): # PUSH - push the value in the given register on the stack
         self.reg[7] -= 1
-        sp = self.reg[7]
-        self.ram[sp] = self.reg[self.ram_read(self.pc + 1)]
+        self.ram[self.reg[7]] = self.reg[self.ram_read(self.pc + 1)]
         self.pc += 2
 
     def handle_pop(self): # POP - pop the value at the top of the stack into the given register
-        sp = self.reg[7]
-        self.reg[self.ram_read(self.pc + 1)] = self.ram[sp]
+        self.reg[self.ram_read(self.pc + 1)] = self.ram[self.reg[7]]
         self.reg[7] += 1
         self.pc += 2
+
+    def handle_call(self):
+        self.reg[7] -= 1
+        self.ram[self.reg[7]] = (self.pc + 2)
+        self.pc = self.reg[self.ram_read(self.pc + 1)]
+
+    def handle_ret(self):
+        self.pc = self.ram[self.reg[7]]
+        self.reg[7] += 1
 
     def run(self):
         """Run the CPU."""
